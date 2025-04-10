979f6e1f13110a52767493ae32f6c39dc98de9f4
AidenHotic12 <AidenHotic12@gmail.com>
2025-04-10 16:18:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// this code links first button "click here" to index2.html
+
+function redirectToNextPage() {
+window.location.href = "index2.html"; 
+}
+
+function redirectToPreviousPage() {
+    window.location.href = "index.html"; 
+}
+
+function redirectToNextPage2() {
+    window.location.href = "index3.html"; 
+}
+
+function redirectToPreviousPage2() {
+    window.location.href = "index2.html"; 
+}
+
+function redirectToNextPage3() {
+    window.location.href = "indexImages.html"; 
+}
+
+function redirectToPreviousFirst() {
+    window.location.href = "index.html"; 
+}
+
+function returnToSearch() {
+    window.location.href = "indexSearch.html"; 
+}
